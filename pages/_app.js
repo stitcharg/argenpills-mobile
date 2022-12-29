246b3be5a2fad42b36e65cfb0cec69b3c08198c9
Stitcharg <97808356+stitcharg@users.noreply.github.com>
2022-12-29 17:55:39 -0300

    Agregado el titulo del site

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import { Fragment } from 'react'
 import '../styles/globals.css'
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const queryClient = new QueryClient();
 
   return (
     <Fragment >
+      <Head>
+        <title>Argenpills Mobile</title>
+      </Head>
       <QueryClientProvider client={queryClient} >
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
