255ed429a2df14b01e0be9b4d671f1ba0dd4ba1e
Stitcharg <97808356+stitcharg@users.noreply.github.com>
2023-01-02 17:22:41 -0300

    Arreglado el dominio

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Logo() {
 	return (
-		<Link href="https://www.argenpills.org"><img src="logo.png" alt="Logo" className="img-fluid" /></Link>
+		<Link href="https://argenpills.org"><img src="logo.png" alt="Logo" className="img-fluid" /></Link>
 	)
 }
 
