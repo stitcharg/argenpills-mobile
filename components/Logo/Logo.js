1cdfc582662a7701df0436cfb527ec6b15b38ec5
milanesaDePechuga <pechugamilanesa@gmail.com>
2025-11-06 17:01:35 -0300

    feat: refactor components for improved layout and loading states; update styles for sticky header and loading overlay.

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -3,10 +3,15 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Logo() {
-	return (
-		<Link href="https://argenpills.org">
-			<Image alt="Logo" className="img-fluid" src="/logo.png" width={311} height={68} />
-		</Link>
-	)
+  return (
+    <Link href="https://argenpills.org">
+      <Image
+        alt="Logo"
+        className="img-fluid"
+        src="/logo.png"
+        width={80}
+        height={80}
+      />
+    </Link>
+  );
 }
-
